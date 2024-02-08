d68aae35dde84f8a4f89ae9324eda5aa34ab1214
Clara Marinho <marinho.claramb@gmail.com>
2024-02-08 17:55:05 -0300

    comp: small devices navbar

---
+++ src/components/smaller_components/NavBar.vue
@@ -0,0 +1,13 @@
+<template>
+    <v-app-bar class="show-sm bg-blue">
+        <template v-slot:prepend>
+            <img src="../../assets/images/unit-logo.webp" alt="">
+        </template>
+    </v-app-bar>
+</template>
+
+<script lang="ts">
+    export default {
+        name: 'NavBar'
+    }
+</script>
